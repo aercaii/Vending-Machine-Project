1426584ea1526958f18dc186d78a2015180e8dd7
Harry Huang <huangharry064@gmail.com>
2025-05-16 17:13:20 -0700

    Item py finished

---
+++ store/item.py
@@ -0,0 +1,21 @@
+class Item:
+    def __init__(self,name,price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def check_stock(self):
+        if self.quantity == 0:
+            print("Out Of Stock")
+        elif self.quantity <0:
+            print("ERR: Stock is under 0")
+        else:
+            print("In Stock")
+    def purchase(self):
+        self.quantity -= 1
+        print(f"Sucessfully purchaed: {self.name} \n Quantity: {self.quantity}")
+        
+    
+        
+        
+    
